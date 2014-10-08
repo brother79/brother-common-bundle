6c4c8678b10ce67dc55aac6484e4423f774b0249
git <brother79@mail.ru>
2014-10-08 09:56:39 +0700

    Fix

--- BrotherCommonBundle.php
+++ BrotherCommonBundle.php
@@ -2,8 +2,17 @@
 
 namespace Brother\CommonBundle;
 
+use Brother\CommonBundle\Error\ErrorLogger;
+use Monolog\ErrorHandler;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class BrotherCommonBundle extends Bundle
 {
+    public function boot()
+    {
+        parent::boot();
+        $logger = $this->container->get('logger');
+        \Monolog\ErrorHandler::register($logger);
+    }
+
 }
