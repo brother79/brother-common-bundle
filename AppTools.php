3e67d09cf405b6f4e667efc8f88ecb8da1fba1a2
git <brother79@mail.ru>
2014-10-17 16:07:07 +0700

    Fix test

--- AppTools.php
+++ AppTools.php
@@ -643,8 +643,12 @@ class AppTools
         if (preg_match('/youtube\.com(?:\/|%2F)watch(?:\/|%2F)?(?:\?|%3F)v(?:=|%3D)([\w-]+)/', $url, $m)) {
             return array('id' => $m[1], 'frame' => '<iframe width="560" height="315" src="//www.youtube.com/embed/' . $params['v'] . '" frameborder="0" allowfullscreen></iframe>');
         }
+        if (strpos($url, 'smartknowledgeu')) {
+            return null;
+        }
         if (strpos($url, 'youtube.')) {
             $r = parse_url($url);
+            AppDebug::_dx($r, $url);
             if (preg_match('/\/channel\/\w+/', $r['path']) ||
                 preg_match('/\/videos/', $r['path']) ||
                 preg_match('/\/user\//', $r['path'])
