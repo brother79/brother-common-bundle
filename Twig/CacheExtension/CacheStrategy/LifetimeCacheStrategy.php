58d4a26d183ab36ada24625c902394e58736ebb7
git <brother79@mail.ru>
2014-12-02 19:51:35 +0600

    Check news

--- Twig/CacheExtension/CacheStrategy/LifetimeCacheStrategy.php
+++ Twig/CacheExtension/CacheStrategy/LifetimeCacheStrategy.php
@@ -25,7 +25,7 @@ use Brother\CommonBundle\AppDebug;
  */
 class LifetimeCacheStrategy implements CacheStrategyInterface
 {
-    const PREFIX='v13_';
+    const PREFIX='v14_';
     private $cache;
 
     /**
