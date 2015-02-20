7939353223f630b455f9015bc9a78146868c9542
git <brother79@mail.ru>
2015-02-20 12:24:56 +0600

    fix cache

--- MongoDB/BaseRepository.php
+++ MongoDB/BaseRepository.php
@@ -159,7 +159,7 @@ class BaseRepository extends DocumentRepository
     }
 
     /**
-     * @param $r \MongoCursor
+     * @param $r \MongoCursor|array
      */
     public function clearCursorCache($r)
     {
