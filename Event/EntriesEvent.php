20326c94c9a1bd85913a2bd424ed0cfa2219480e
git <brother79@mail.ru>
2015-07-16 10:53:05 +0600

    Map

---
+++ Event/EntriesEvent.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Brother\CommonBundle\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * Base class for an event that occurs related to performing 
+ * a batch operation on a list of guestbook entries.
+ */
+class EntriesEvent extends Event
+{
+    private $ids;
+
+    /**
+     * Constructs an event.
+     *
+     * @param array $ids
+     */
+    public function __construct($ids)
+    {
+        $this->ids = $ids;
+    }
+	
+    /**
+     * Returns the entry ids for this event.
+     *
+     * @return array
+     */
+    public function getIds()
+    {
+        return $this->ids;
+    }
+}
