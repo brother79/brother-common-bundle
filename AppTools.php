8c4b9dfdb78bf90709fb44a5576cdb74842a36dd
brother79 <brother79@mail.ru>
2018-12-10 16:39:46 +0700

    Тесты, баг кодировки, определение кодировки

--- AppTools.php
+++ AppTools.php
@@ -911,13 +911,13 @@ class AppTools {
     }
 
     public static function detectEncoding($html) {
-        if ($t = iconv('utf-8', 'utf-8', $html)) {
+        if ($t = @iconv('utf-8', 'utf-8', $html)) {
             return $html;
         }
-        if ($t = iconv('utf8', 'utf-8', $html)) {
+        if ($t = @iconv('utf8', 'utf8', $html)) {
             return $html;
         }
-        if ($t = iconv('cp1251', 'cp1251', $html)) {
+        if ($t = @iconv('cp1251', 'cp1251', $html)) {
             return iconv('cp1251', 'utf-8', $html);
         }
         return $html;
